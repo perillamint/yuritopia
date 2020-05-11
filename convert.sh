8d9fbc4ae8fd2572b79a467c80e978d238debf58
perillamint <perillamint@quendi.moe>
2020-05-12 07:26:19 +0900

    Fix typo

--- convert.sh
+++ convert.sh
@@ -11,9 +11,9 @@ VIDEOBITRATE="756k"
 
 for i in *.rpa; do
 	mkdir -p "$i-unpack"
-	cd "$i-unpack"
+	pushd "$i-unpack"
 	unrpa "../$i"
-	fd -j $NTHREAD --glob "*.png" -x ../../transcode-image.sh {} \;
+	fd -j $NTHREAD --glob "*.png" -x ../../util/transcode-image.sh {} \;
 	fd -j $NTHREAD --glob "*.webm" -x ../../util/transcode.sh {} mkv -c:v libvpx -b:v "$VIDEOBITRATE" -c:a libopus -b:a "$AUDIOBITRATE" \;
 	if [[ "$i" == voice* ]]; then
 		AUDIOBITRATE="64k"
@@ -21,7 +21,7 @@ for i in *.rpa; do
 	fd -j $NTHREAD --glob "*.opus" -x ../../util/transcode.sh {} webm -c:a libopus -b:a "$AUDIOBITRATE" \;
 	rm "../$i"
 	rpatool -2 -c "../$i" .
-	cd ..
+	popd
 	rm -rfv "$i-unpack"
 done
 
